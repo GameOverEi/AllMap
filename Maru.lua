83d2e41f08bfbce23631cfe1b576911d012de41f
XsaDiSm <94647479+GameOverEi@users.noreply.github.com>
2024-01-08 16:53:47 +0700

    Maru.lua

--- Maru.lua
+++ Maru.lua
@@ -1,9 +1,4 @@
 
-repeat task.wait() until game:IsLoaded()
-repeat task.wait() until game.Players
-repeat task.wait() until game.Players.LocalPlayer
-repeat task.wait() until game.Players.LocalPlayer:FindFirstChild("PlayerGui")
-repeat task.wait() until game.Players.LocalPlayer.PlayerGui:FindFirstChild("Main");
 _G.Team = "Pirate" -- Marine / Pirate
 _G.KAITUN_SCRIPT = true
 _G.LogsDes = {
